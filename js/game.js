ec77da0e75862669e5d62f15c09bb42cde90108b
Tinnaphop Choonchuachan <thathobotfy64@gmail.com>
2022-10-07 11:05:07 +0700

    first commit

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 var player = {
-	cash: new Decimal("0"),
+	cash: new Decimal("1"),
 	lastUpdateTime: Date.now(),
 	generators: [
 		{},
@@ -9,40 +9,32 @@ var player = {
 			multiplier: new Decimal("1"),
 		},
 	],
+	cashUpgrades: {
+		"cu0": {},
+		"cu1": {
+			level: new Decimal("0"),
+			maximumLevel: null, // i.e., no limit
+			currentEffect: new Decimal("1"),
+		},
+	},
 };
 
+const SCREENS = [
+	"cash-generator-purchasing-area-container",
+	"cash-upgrade-purchasing-area-container",
+];
+
 function buyGenerator(number) {
-	// if (
-	// 	(number === 1 && player.generators[1].amountPurchased.eq(0)) ||
-	// 	player.generators[1] === undefined
-	// ) {
-	// 	player.generators[1].amountPurchased = new Decimal("0");
-	// 	player.generators[1].amountPurchased =
-	// 		player.generators[1].amountPurchased.add("1");
-	// 	player.generators[1].amountOwned =
-	// 		player.generators[1].amountOwned.add("1");
-	// 	if (player.generators[number].amountPurchased.eq(1)) {
-	// 		writeNewGenerator(number + 1);
-	// 	}
-	// 	player.generators[1].multiplier = player.generators[1].multiplier.mul(2);
-	// } else {
-		if (
-			player.cash.gte(
-				getCostForGenerator(number)
-			)
-		) {
-			player.cash = player.cash.sub(getCostForGenerator(number))
-			player.generators[number].amountPurchased =
-				player.generators[number].amountPurchased.add("1");
-			player.generators[number].amountOwned =
-				player.generators[number].amountOwned.add("1");
-			if (player.generators[number].amountPurchased.eq(1)) {
-				writeNewGenerator(number + 1);
-			}
-		for (let i = 1; i <= number; i++){
-			player.generators[number].multiplier = player.generators[number].multiplier.mul(number+1);
-		}}
-		
+	if (player.cash.gte(getCostForGenerator(number))) {
+		player.cash = player.cash.sub(getCostForGenerator(number));
+		player.generators[number].amountPurchased =
+			player.generators[number].amountPurchased.add("1");
+		player.generators[number].amountOwned =
+			player.generators[number].amountOwned.add("1");
+		if (player.generators[number].amountPurchased.eq(1)) {
+			writeNewGenerator(number + 1);
+		}
+	}
 }
 
 function writeNewGenerator(number) {
@@ -51,14 +43,63 @@ function writeNewGenerator(number) {
 		amountOwned: new Decimal("0"),
 		multiplier: new Decimal("1"),
 	};
-	$(`#generator-purchasing-area-container`).append(
-		`<div class="generator-information-container"><span>Tier ${number} Generators - Bought: <span id="generator-${number}-purchased-count">0</span></span><span id="generator-${number}-owned-count">0</span><button class="button--large-rectangle button--generator" onClick="buyGenerator(${number})">Buy 1 for <span id="generator-${number}-cost">0</button></div>`
+	$(`#cash-generator-purchasing-area-container`).append(
+		`<div class="cash-generator-information-container"><span>Tier ${number} Cash Generators &#10005;<span id="cash-generator-${number}-multiplier">1</span></span><span id="cash-generator-${number}-owned-count">0</span><button class="button--large-rectangle button--generator" onClick="buyGenerator(${number})">Buy 1 for <span id="cash-generator-${number}-cost">0</button></div>`
 	);
 }
 
-function getCostForGenerator(number){
-	if (number === 1 && player.generators[1].amountPurchased.eq(0)){
-		return new Decimal("0");
+function getCostForGenerator(number) {
+	if (number === 1 && player.generators[1].amountPurchased.eq(0)) {
+		return new Decimal("1");
 	}
-	return new Decimal(number).pow(new Decimal(number).root(3-(number-2)*0.1 > 1 ? 3-(number-2)*0.1 : 1  )).mul(new Decimal(4).pow(number)).pow(player.generators[number].amountPurchased.add("1"));
+	return new Decimal(number)
+		.pow(
+			new Decimal(number).root(
+				3 - (number - 2) * 0.1 > 1 ? 3 - (number - 2) * 0.1 : 1
+			)
+		)
+		.mul(new Decimal(4).pow(number))
+		.pow(
+			player.generators[number]?.amountPurchased.add("1") ??
+				new Decimal("1")
+		);
 }
+
+function getCostForCashUpgrade(number) {
+	return new Decimal(10).pow(
+		player.cashUpgrades[`cu${number}`].level
+			.mul(new Decimal("3").add(number))
+			.add(10)
+	);
+}
+
+function buyCashUpgrade(number) {
+	if (player.cashUpgrades[`cu${number}`].maximumLevel) {
+		// has limit
+		if (
+			player.cashUpgrades[`cu${number}`].level.lt(maximumLevel) &&
+			player.cash.gte(getCostForCashUpgrade(number))
+		) {
+			player.cash = player.cash.sub(number);
+			player.cashUpgrades[`cu${number}`].level =
+				player.cashUpgrades[`cu${number}`].level.add(1);
+			return;
+		}
+	}
+	// doesn't have limit
+	if (player.cash.gte(getCostForCashUpgrade(number))) {
+		player.cash = player.cash.sub(number);
+		player.cashUpgrades[`cu${number}`].level =
+			player.cashUpgrades[`cu${number}`].level.add(1);
+		return;
+	}
+}
+
+function navigateTo(screen) {
+	for (let i = 0; i < SCREENS.length; i++) {
+		$(`#${SCREENS[i]}`).hide(0);
+	}
+	$(`#${screen}`).show(0);
+}
+
+navigateTo("cash-generator-purchasing-area-container");
--- js/loop.js
+++ js/loop.js
@@ -3,24 +3,65 @@ setInterval(() => {
 	let currentTime = Date.now();
 	let deltaTimeInMilliseconds = currentTime - player.lastUpdateTime;
 	player.lastUpdateTime = currentTime;
-	
 
-	for (let i = 1; i <= player.generators.length-1; i++){
-		if (i === 1){
-			player.cash = player.cash.add(player.generators[1].amountOwned.mul(deltaTimeInMilliseconds/1000).mul(player.generators[1].multiplier));
-		} else {
-			player.generators[i-1].amountOwned = player.generators[i-1].amountOwned.add(player.generators[i].amountOwned.mul(deltaTimeInMilliseconds/1000).mul(player.generators[1].multiplier));
-		}
-
+	for (let i = 1; i <= player.generators.length - 1; i++) {
+		player.generators[i].multiplier = player.generators[
+			i
+		].amountPurchased.eq(0)
+			? new Decimal("1")
+			: new Decimal("1.1").pow(
+					player.generators[i].amountPurchased.sub(1)
+			  );
 	}
+
+	for (let i = 1; i <= player.generators.length - 1; i++) {
+		if (i === 1) {
+			player.cash = player.cash.add(
+				player.generators[1].amountOwned
+					.mul(deltaTimeInMilliseconds / 1000)
+					.mul(player.generators[1].multiplier)
+					.mul(player.cashUpgrades[`cu1`].currentEffect) // cu1 effect
+			);
+		} else {
+			player.generators[i - 1].amountOwned = player.generators[
+				i - 1
+			].amountOwned.add(
+				player.generators[i].amountOwned
+					.mul(deltaTimeInMilliseconds / 1000)
+					.mul(player.generators[1].multiplier)
+			);
+		}
+	}
+
+	// update variables
+	player.cashUpgrades["cu1"].currentEffect = new Decimal("1.5").pow(
+		player.cashUpgrades["cu1"].level
+	);
 
 	// update html
 	$("#cash-count").text(formatNumber(player.cash));
 
-	for (let i = 1; i <= player.generators.length-1; i++){
-		$(`#generator-${i}-purchased-count`).text(player.generators[i].amountPurchased.toFixed(0))
-		$(`#generator-${i}-owned-count`).text(formatNumber(player.generators[i].amountOwned))
-		$(`#generator-${i}-cost`).text(formatNumber(getCostForGenerator(i)));
+	for (let i = 1; i <= player.generators.length - 1; i++) {
+		$(`#cash-generator-${i}-multiplier`).text(
+			formatNumber(player.generators[i].multiplier)
+		);
+		$(`#cash-generator-${i}-owned-count`).text(
+			formatNumber(player.generators[i].amountOwned)
+		);
+		$(`#cash-generator-${i}-cost`).text(
+			formatNumber(getCostForGenerator(i))
+		);
 	}
 
+	for (let i = 1; i <= Object.keys(player.cashUpgrades).length - 1; i++) {
+		$(`#cash-upgrade-${i}__current-effect`).text(
+			formatNumber(player.cashUpgrades[`cu${i}`].currentEffect)
+		);
+		$(`#cash-upgrade-${i}__level`).text(
+			player.cashUpgrades[`cu${i}`].level
+		);
+		$(`#cash-upgrade-${i}__cost`).text(
+			formatNumber(getCostForCashUpgrade(i))
+		);
+	}
 }, 50);
